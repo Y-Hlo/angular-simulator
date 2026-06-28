acc6f73ea2d17810fd6c72e8df931b14d296e296
Y-Hlo <hlo.yuri@gmail.com>
2026-06-28 13:44:58 +0300

    feat: complete homework 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-// 2.2. Подключения файла "training.ts"
-
+import { Collection } from '../collection';
+import { Color } from '../enums/Color';
 import './training';
 import { Component } from '@angular/core';
 
@@ -10,5 +10,44 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  companyName: string = 'РУМТИБЕТ';
+
+  constructor() {
+
+// 15. п 3.1. Вызов метода сохранения даты при загрузке страницы
+
+    this.saveLastVisit();
+
+// 15. п 4.1. Вызов метода сохранения количества визитов при загрузке страницы
+
+    this.saveVisitCount();
+  }
+
+// 15. п 2.1. Метод проверки цвета на принадлежность к основным
+
+  checkColor(color: Color): boolean {
+    return color === Color.Red || color === Color.Green || color === Color.Blue;
+  }
+
+// 15. п 3. Метод сохранения даты последнего визита в LocalStorage
+
+  saveLastVisit(): void {
+    const currentDate = new Date().toISOString();
+    localStorage.setItem('lastVisit', currentDate);
+  }
+
+// 15. п 4. Метод сохранения количества визитов в LocalStorage
+
+  saveVisitCount(): void {
+    const currentVisits = Number(localStorage.getItem('visitCount')) || 0;
+    localStorage.setItem('visitCount', String(currentVisits + 1));
+  }
+
+// 15. п 5.6. Реализация универсальной коллекции с двумя разными типами данных
+
+stringCollection = new Collection<string>();
+
+numberCollection = new Collection<number>();
 
 }
+
