0bd7bd63e0f98e55208b4ec61a32e59f13ebebf4
Y-Hlo <hlo.yuri@gmail.com>
2026-07-31 13:28:27 +0300

    feat: complete homework 19

---
+++ src/message.service.ts
@@ -0,0 +1,39 @@
+// 19. п 3.3. Сервис управления сообщениями
+
+import { Injectable } from '@angular/core';
+import { IMessage } from './interfaces/IMessage';
+import { MessageType } from './enums/MessageType';
+
+@Injectable()
+
+export class MessageService {
+  private items: IMessage[] = [];
+
+  get messages(): IMessage[] {
+    return this.items;
+  }
+
+  // 19. п 3.4. Метод добавления нового сообщения в список с авто-скрытием
+
+  addMessage(type: MessageType, text: string): void {
+    const newMessage: IMessage = {
+      id: this.items.length + 1,
+      type: type,
+      text: text,
+    };
+
+    this.items.unshift(newMessage);
+
+    setTimeout(() => {
+      this.closeMessage(newMessage.id);
+    }, 5000);
+
+  }
+
+  // 19. п 3.5. Метод закрытия (удаления) сообщения по его ID
+
+  closeMessage(id: number): void {
+    this.items = this.items.filter(item => item.id !== id);
+  }
+
+}
