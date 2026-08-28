a24365b3b7acfcc49e32142d971965de47750517
Y-Hlo <hlo.yuri@gmail.com>
2026-08-28 12:13:16 +0300

    feat: complete homework 20

--- src/message.service.ts
+++ src/message.service.ts
@@ -1,6 +1,6 @@
 // 19. п 3.3. Сервис управления сообщениями
 
-import { Injectable } from '@angular/core';
+import { Injectable, Type } from '@angular/core';
 import { IMessage } from './interfaces/IMessage';
 import { MessageType } from './enums/MessageType';
 
@@ -17,7 +17,7 @@ export class MessageService {
 
   // 19. п 3.4. Метод добавления нового сообщения в список с авто-скрытием
 
-  addMessage(type: MessageType, text: string): void {
+  private addMessage(type: MessageType, text: string): void {
     const newMessage: IMessage = {
       id: this.currentId++,
       type: type,
@@ -36,6 +36,24 @@ export class MessageService {
 
   closeMessage(id: number): void {
     this.items = this.items.filter(item => item.id !== id);
+  }
+
+  // 20. п 3.4. Добавление в сервис методов: showWarn, showError, showSuccess, showInfo
+
+  showWarn(text: string): void {
+    this.addMessage(MessageType.Warn, text)
+  }
+  
+  showError(text: string): void {
+    this.addMessage(MessageType.Error, text)
+  }
+  
+  showSuccess(text: string): void {
+    this.addMessage(MessageType.Success, text)
+  } 
+  
+  showInfo(text: string): void {
+    this.addMessage(MessageType.Info, text)
   }
 
 }
